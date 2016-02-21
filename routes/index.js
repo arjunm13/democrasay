77cb27a2d48056cff876f4bbee685d7db6b07b9c
arjunm13 <arjunmahen13@gmail.com>
2016-02-21 17:16:13 -0500

    last minute

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var agree=0; 
+var disagree=0;
+var agreepercent = 0;
+var disagreepercent = 0;
+var total=0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', { title: 'Express', 
+agree: agreepercent,
+disagree: disagreepercent});
 });
+
+router.get('/agree', function(req, res, next) {
+	total = total +1;
+	agree = agree +1;
+	disagreepercent = (disagree / total)*100;
+	agreepercent = (agree / total)*100;
+	disagreepercent = disagreepercent.toFixed(2);
+	agreepercent= agreepercent.toFixed(2);
+
+	console.log(agree);
+  res.render('index', { title: 'Express', 
+agree: agreepercent,
+disagree: disagreepercent});
+});
+router.get('/disagree', function(req, res, next) {
+	total = total +1;
+	disagree = disagree +1;
+	agreepercent = (agree / total)*100;
+	disagreepercent = (disagree / total)*100;
+	disagreepercent = disagreepercent.toFixed(2);
+	agreepercent= agreepercent.toFixed(2);
+	console.log(disagree);
+  res.render('index', { title: 'Express', 
+agree: agreepercent,
+disagree: disagreepercent});
+});
+
+
 
 module.exports = router;
